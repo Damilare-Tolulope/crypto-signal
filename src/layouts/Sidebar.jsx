bb6e4d2d967b96f52ad8ecd921781b92e87983cd
Damilare Tolulope <damilaretolulope1@gmail.com>
2023-04-03 14:35:19 +0100

    fix: fix sidebar

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -2,27 +2,47 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { links } from "../data";
 
+import night from '../assets/icons/night.svg'
+
 const Sidebar = () => {
   return (
     <aside className={styles.aside}>
       <ul className={styles.links}>
         {links.slice(0,4).map(({ icon, label, route }) => (
-          <NavLink className={({ isActive }) => ( isActive ? styles.activeLink : styles.nLink )} key={label} to={route}>
-            <li className={styles.link}>
-              <img src={icon} alt={label} />
-              <span>{label}</span>
-            </li>
-          </NavLink>
+          <li>
+            <NavLink className={({ isActive }) => ( isActive ? styles.activeLink : styles.link )} key={label} to={route}>
+                <img src={icon} alt={label} />
+                <span>{label}</span>
+            </NavLink>
+          </li>
         ))}
-        <div className="border-b border-white/5 my-2"></div>
-        {links.slice(4,6).map(({ icon, label, route }) => (
-          <NavLink className={({ isActive }) => ( isActive ? styles.activeLink : styles.nLink )} key={label} to={route}>
-            <li className={styles.link}>
-              <img src={icon} alt={label} />
-              <span>{label}</span>
-            </li>
-          </NavLink>
-        ))}
+        <div className="border-b border-white/5 my-5"></div>
+        {links.slice(4,6).map(({ icon, label, route }, index) => {
+          if(index === 1){
+            return (
+              <li>
+                <NavLink className={({ isActive }) => ( isActive ? styles.activeLink : `${styles.link} !text-primary` )} key={label} to={route}>
+                    <img src={icon} alt={label} />
+                    <span>{label}</span>
+                </NavLink>
+              </li>
+            )
+          }else {
+            return (
+              <li>
+                <NavLink className={({ isActive }) => ( isActive ? styles.activeLink : styles.link )} key={label} to={route}>
+                    <img src={icon} alt={label} />
+                    <span>{label}</span>
+                </NavLink>
+              </li>
+            )
+          }
+          })}
+        <div className="border-b border-white/5 my-5"></div>
+          <li className={styles.link}>
+            <img src={night} alt="night" />
+            <span>Dark Mode</span>
+          </li>
       </ul>
     </aside>
   );
@@ -31,9 +51,8 @@ const Sidebar = () => {
 const styles = {
     aside: "bg-dark w-1/5 px-5 h-full fixed",
     links: "py-5",
-    nLink: "text-white/40 px-5 py-2",
-    link: "flex items-center gap-3 font-medium px-5 my-2 transition linear duration-300 hover:text-primary",
-    activeLink: "flex items-center py-2 transition linear duration-300 gap-3 font-bold ml-5 border-l-2 border-primary text-primary"
+    link: "flex my-1 cursor-pointer items-center gap-3 text-white/40 font-medium py-3 px-5 transition linear duration-300 hover:text-primary",
+    activeLink: "flex items-center py-3 px-5 transition linear duration-300 gap-3 font-bold ml-5 border-l-2 border-primary text-primary"
 }
 
 export default Sidebar;
